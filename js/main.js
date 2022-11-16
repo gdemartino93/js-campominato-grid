b9122641837fd0700ec415071b76e24a256debf7
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2022-11-16 15:40:31 +0100

    adding cell for each difficulty value

--- js/main.js
+++ js/main.js
@@ -1,8 +1,23 @@
 const BTNPLAY=document.getElementById("play");
-const CONTAINER= document.querySelector(".container")
+const CONTAINER= document.querySelector(".container");
+const SCELTA= document.getElementById("difficult");
+// dichiaro tasto play, container e la scelta della difficoltà
+
+let valueDifficult= 0;
+// dichiaro valore di difficoltà
+if (SCELTA.value == "ez"){
+    valueDifficult = 101;
+}
+if (SCELTA.value == "normal"){
+    valueDifficult= 51;
+}
+if (SCELTA.value == "hard"){
+    valueDifficult= 31;
+}
 // dichiaro tasto e container
 
-for( let i = 1; i < 101; i++){
+
+for( let i = 1; i < valueDifficult; i++){
     let box= document.createElement("div");
     box.innerHTML=`${i}`
     BTNPLAY.addEventListener("click",
