f9df47ff3586a66cb67118a96dc859714cd85281
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2022-11-16 17:07:34 +0100

    fix layout vh

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-const BTNPLAY=document.getElementById("play");
-const CONTAINER= document.querySelector(".container");
-const SCELTA= document.getElementById("difficult");
+const BTNPLAY = document.getElementById("play");
+const CONTAINER = document.querySelector(".container");
+const SCELTA = document.getElementById("difficult");
 
 // dichiaro tasto play, container e la scelta della difficoltà
 
@@ -8,42 +8,41 @@ const SCELTA= document.getElementById("difficult");
 
 BTNPLAY.addEventListener("click",
     function(){
-        CONTAINER.innerHTML=""
+        CONTAINER.innerHTML = ""
         let valueDifficult= 0;
         if (SCELTA.value == "ez"){
             valueDifficult = 101;
         }
         if (SCELTA.value == "normal"){
-            valueDifficult= 82;
+            valueDifficult = 82;
         }
         if (SCELTA.value == "hard"){
-            valueDifficult= 50;
+            valueDifficult = 50;
         }
         for( let i = 1; i < valueDifficult; i++){
             let box= document.createElement("div");
-            box.innerHTML=`${i}`
+            box.innerHTML = `${i}`
             CONTAINER.append(box)
             box.classList.add("boxstyle")
             
 
-    box.addEventListener("click",
-    function(){
-        console.log(i)
-        this.classList.toggle("active")
-    })
-    if (SCELTA.value == "ez"){
-        valueDifficult = 101;
-        box.classList.add("width-easy")
-        
-    }
-    if (SCELTA.value == "normal"){
-        valueDifficult= 82;
-        box.classList.add("width-mid")
-    }
-    if (SCELTA.value == "hard"){
-        valueDifficult= 50;
-        box.classList.add("width-hard")
-    }
-}
-        
+            box.addEventListener("click",
+            function(){
+                console.log(i)
+                this.classList.toggle("active")
+            })
+
+            if (SCELTA.value == "ez"){
+                valueDifficult = 101;
+                box.classList.add("width-easy")
+            }
+            if (SCELTA.value == "normal"){
+                valueDifficult = 82;
+                box.classList.add("width-mid")
+            }
+            if (SCELTA.value == "hard"){
+                valueDifficult = 50;
+                box.classList.add("width-hard")
+            }
+        }
 })
