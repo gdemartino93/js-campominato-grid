4fdb5035d7d7694a15a131e17d911ed1391b46b0
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2022-11-16 13:54:04 +0100

    btn play and js cmd to generate grid

--- js/main.js
+++ js/main.js
@@ -2,9 +2,12 @@ const BTNPLAY=document.getElementById("play");
 const CONTAINER= document.querySelector(".container")
 // dichiaro tasto e container
 
-
 for( let i = 0; i < 80; i++){
     let box= document.createElement("div");
-    CONTAINER.append(box)
-    box.classList.add("boxstyle")
+    BTNPLAY.addEventListener("click",
+        function(){
+            CONTAINER.append(box)
+            box.classList.add("boxstyle")
+        }
+    )
 }
